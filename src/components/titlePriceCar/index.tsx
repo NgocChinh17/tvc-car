6a468c253a335bc5edc87fb7e0463660113586e0
NgocChinh17 <chinhwedit@gmail.com>
2025-04-22 10:59:15 +0700

    padding font-size

--- src/components/titlePriceCar/index.tsx
+++ src/components/titlePriceCar/index.tsx
@@ -1,25 +1,16 @@
-import React from 'react'
+import React from "react";
+
+import './style.scss'
 
 interface SectionHeaderProps {
-    title: string;
-  }
-
-export const TitlePriceCar: React.FC<SectionHeaderProps> = ({title}) => {
-  return (
-    <>
-    <div style={{ display: "flex", alignItems: "center", marginBlock: 16 }}>
-        <div
-          style={{
-            width: "4px",
-            height: "20px",
-            backgroundColor: "#e5e5e5",
-            marginRight: "10px",
-          }}
-        />
-        <h4 style={{ color: "#ff6600", fontWeight: "bold", marginBlock: 0 }}>
-          <p>{title}</p>
-        </h4>
-      </div>
-    </>
-  )
+  title: string;
 }
+
+export const TitlePriceCar: React.FC<SectionHeaderProps> = ({ title }) => {
+  return (
+    <div style={{ display: "flex", alignItems: "center", marginBlock: 16 }}>
+      <div className="line-header" />
+      <h4 className="title-line-header">{title}</h4>
+    </div>
+  );
+};
